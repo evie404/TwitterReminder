d29fac881004a9cbd76fa3e98a9cf64b027ab8fd
Ching-Yen Pai <ricky@aniarc.com>
2012-12-02 09:53:35 -0800

    cap testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,53 @@
-# -*- encoding : utf-8 -*-
-default_environment["PATH"] = "/opt/ruby/bin:/usr/local/bin:/usr/bin:/bin"
+require "bundler/capistrano"
+
+server "ec2-54-245-75-232.us-west-2.compute.amazonaws.com", :web, :app, :db, primary: true
 
 set :application, "TwitterReminder"
-set :repository,  "git@github.com:example/#{application}.git"
-set :deploy_to, "/home/apps/#{application}"
-
-set :branch, "master"
-set :scm, :git
-
-set :user, "apps"
-set :group, "apps"
-
-set :deploy_to, "/home/apps/#{application}"
-set :runner, "apps"
+set :user, "root"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :git_shallow_clone, 1
-
-role :web, "TwitterReminder.com"                          # Your HTTP server, Apache/etc
-role :app, "TwitterReminder.com"                         # This may be the same as your `Web` server
-role :db,  "TwitterReminder.com"   , :primary => true # This is where Rails migrations will run
-
-set :deploy_env, "production"
-set :rails_env, "production"
-set :scm_verbose, true
 set :use_sudo, false
 
+set :scm, "git"
+set :repository, "git@github.com:rickypai/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-
-  desc "Restart passenger process"
-  task :restart, :roles => [:web], :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
-
-
-namespace :my_tasks do
-  task :symlink, :roles => [:web] do
-    run "mkdir -p #{deploy_to}/shared/log"
-    run "mkdir -p #{deploy_to}/shared/pids"
-    
-    symlink_hash = {
-      "#{shared_path}/config/database.yml"   => "#{release_path}/config/database.yml",
-      "#{shared_path}/config/s3.yml"   => "#{release_path}/config/s3.yml",
-      "#{shared_path}/uploads"              => "#{release_path}/public/uploads",
-    }
-
-    symlink_hash.each do |source, target|
-      run "ln -sf #{source} #{target}"
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
-end
-
-
-
-namespace :remote_rake do
-  desc "Run a task on remote servers, ex: cap staging rake:invoke task=cache:clear"
-  task :invoke do
-    run "cd #{deploy_to}/current; RAILS_ENV=#{rails_env} bundle exec rake #{ENV['task']}"
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/config.yml"), "#{shared_path}/config/config.yml"
+    puts "Now edit the config files in #{shared_path}."
   end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
-
-after "deploy:finalize_update", "my_tasks:symlink"
-
