d8f32343781b8c6da90a99870d45d444ce4cc86e
Ching-Yen Pai <ricky@aniarc.com>
2012-12-01 16:16:31 -0800

    worker now reschedules itself after completion

--- app/workers/tweet_checker_worker.rb
+++ app/workers/tweet_checker_worker.rb
@@ -3,15 +3,14 @@ class TweetCheckerWorker
   sidekiq_options queue: "tweet_check"
     
   def perform
-    # snippet = Snippet.find(snippet_id)
-    # uri = URI.parse("http://pygments.appspot.com/")
-    # request = Net::HTTP.post_form(uri, lang: snippet.language, code: snippet.plain_code)
-    # snippet.update_attribute(:highlighted_code, request.body)
-
     User.all.each do |user|
-      user.update_tweets(50)
+      if user.tweets.count == 0
+        user.update_tweets(200)
+      else
+        user.update_tweets(50)
+      end
     end
 
-    #self.perform_in(5.minutes)
+    self.class.perform_in(5.minutes)
   end
 end
